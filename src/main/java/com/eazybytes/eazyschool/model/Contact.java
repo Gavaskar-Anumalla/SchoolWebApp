51db50ab5b52d920cd6e781ba8a700d5ed84516f
Anumalla Gavaskar <anumallagavaskar@gmail.com>
2025-06-01 10:27:08 +0530

    Added validations in contact page

--- src/main/java/com/eazybytes/eazyschool/model/Contact.java
+++ src/main/java/com/eazybytes/eazyschool/model/Contact.java
@@ -1,5 +1,9 @@
 package com.eazybytes.eazyschool.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 /*
@@ -9,12 +13,32 @@ This makes our code short and clean.
 * */
 @Data
 public class Contact {
+    /*
+       * @NotNull: Checks if a given field is not null but allows empty values & zero elements inside collections.
+         @NotEmpty: Checks if a given field is not null and its size/length is greater than zero.
+         @NotBlank: Checks if a given field is not null and trimmed length is greater than zero.
+       * */
 
+    @NotBlank(message = "Name must not be Blank")
+    @Size(min = 3,message = "Name must be at least 3 characters long")
     private String name;
+
+    @NotBlank(message = "MobileNum must not be Blank")
+    @Pattern(regexp = "(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
     private String mobileNum;
+
+    @NotBlank(message = "Email must not be Blank")
+    @Email(message = "Please provide a valid email address")
     private String email;
+
+    @NotBlank(message = "Subject must not be Blank")
+    @Size(min=5, message="Subject must be at least 5 characters long")
     private String subject;
+
+    @NotBlank(message = "Message must not be Blank")
+    @Size(min=10, message="Message must be at least 10 characters long")
     private String message;
+
     /*public String getName() {
         return name;
     }
