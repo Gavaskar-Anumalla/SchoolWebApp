dd9424359457b6d33f79809c6f73b36eb916b4ab
Anumalla Gavaskar <anumallagavaskar@gmail.com>
2025-06-08 19:23:47 +0530

    Introduction to spring security - permitAll() , denyAll()

---
+++ src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.eazybytes.eazyschool.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        //permit all paths inside web application
+        //1-Approach
+        /*http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests.anyRequest().permitAll());
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());*/
+
+        //2-Approach
+        http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests.anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+        //deny all requests inside web application
+       /* http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests.anyRequest().denyAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());*/
+
+        return http.build();
+    }
+}
--- src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -4,16 +4,12 @@ import com.eazybytes.eazyschool.model.Contact;
 import com.eazybytes.eazyschool.service.ContactService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
