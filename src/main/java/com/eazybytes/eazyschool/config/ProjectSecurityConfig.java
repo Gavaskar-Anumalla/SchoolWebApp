4c971d9fc2bc599dd1557699cc50bcfa23e39ac4
Anumalla Gavaskar <anumallagavaskar@gmail.com>
2025-06-29 12:33:38 +0530

    IN-MEMORY AUTHENTICATION IN SPRING SECURITY

--- src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -41,7 +44,7 @@ public class ProjectSecurityConfig {
                 .httpBasic(Customizer.withDefaults());*/
 
         //Customize the security Configurations
-        http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests.requestMatchers("/","/home").permitAll()
+        http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests.requestMatchers("/","/home").authenticated()
                         .requestMatchers("/holidays/**").permitAll()
                         .requestMatchers("/contact").permitAll()
                         .requestMatchers("/saveMsg").permitAll()
@@ -53,5 +56,22 @@ public class ProjectSecurityConfig {
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails user= User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .roles("USER")
+                .build();
+
+        UserDetails admin= User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("54321")
+                .roles("USER","ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+
     }
 }
